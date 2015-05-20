5c397a2fa58b1fe19369887b5094f572d65db4b2
Jasper St. Pierre <jstpierre@mecheye.net>
2015-05-20 15:48:50 -0700

    main: Use a g_autoptr to clean up the daemon

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 
 int main (int argc, char *argv[])
 {
-  EcmDaemon *daemon = g_object_new (ECM_TYPE_DAEMON,
-                                    "application-id", "com.endlessm.CompositeMode",
-                                    "flags", G_APPLICATION_IS_SERVICE,
-                                    NULL);
+  g_autoptr(EcmDaemon) daemon = g_object_new (ECM_TYPE_DAEMON,
+                                              "application-id", "com.endlessm.CompositeMode",
+                                              "flags", G_APPLICATION_IS_SERVICE,
+                                              NULL);
   return g_application_run (G_APPLICATION (daemon), argc, argv);
 }
