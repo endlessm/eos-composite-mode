74fef2ce86e2a8a4b80f96a4e71ec7363fcacce8
Mario Sanchez Prada <mario@endlessm.com>
2016-01-08 15:04:56 +0000

    Use defines for common strings for GSetting schemas and keys
    [endlessm/eos-shell#6197]

--- src/ecm-settings.c
+++ src/ecm-settings.c
@@ -1,6 +1,12 @@
 
 /* This file is hackily part of ecm-daemon.c */
 
+#define COMPOSITE_MODE_RELOCATABLE_SCHEMA "com.endlessm.CompositeMode.mode"
+#define GNOME_DESKTOP_INTERFACE_SCHEMA "org.gnome.desktop.interface"
+
+#define INITIALIZED_KEY "initialized"
+#define TEXT_SCALING_FACTOR_KEY "text-scaling-factor"
+
 static const char *
 get_path (EcmState state)
 {
@@ -14,48 +20,52 @@ get_path (EcmState state)
 static GSettings *
 get_state_settings (EcmState state)
 {
-  return g_settings_new_with_path ("com.endlessm.CompositeMode.mode", get_path (state));
+  return g_settings_new_with_path (COMPOSITE_MODE_RELOCATABLE_SCHEMA, get_path (state));
 }
 
 static void
 ecm_settings_persist (EcmState old_state)
 {
-  g_autoptr (GSettings) interface_settings = g_settings_new ("org.gnome.desktop.interface");
+  g_autoptr (GSettings) interface_settings = g_settings_new (GNOME_DESKTOP_INTERFACE_SCHEMA);
 
   /* Persist old values in the old state */
   g_autoptr (GSettings) old_state_settings = get_state_settings (old_state);
-  g_settings_set_double (old_state_settings, "text-scaling-factor", g_settings_get_double (interface_settings, "text-scaling-factor"));
+  g_settings_set_double (old_state_settings,
+                         TEXT_SCALING_FACTOR_KEY,
+                         g_settings_get_double (interface_settings, TEXT_SCALING_FACTOR_KEY));
 }
 
 static void
 ecm_settings_load (EcmState new_state)
 {
-  g_autoptr (GSettings) interface_settings = g_settings_new ("org.gnome.desktop.interface");
+  g_autoptr (GSettings) interface_settings = g_settings_new (GNOME_DESKTOP_INTERFACE_SCHEMA);
 
   /* Now load values from the new state into the session */
   g_autoptr (GSettings) new_state_settings = get_state_settings (new_state);
 
-  if (!g_settings_get_boolean (new_state_settings, "initialized"))
+  if (!g_settings_get_boolean (new_state_settings, INITIALIZED_KEY))
     {
       /* If we've never used composite before, then load the default values for that. */
       if (new_state == ECM_STATE_COMPOSITE)
         {
-          g_settings_set_double (new_state_settings, "text-scaling-factor", 1.1);
+          g_settings_set_double (new_state_settings, TEXT_SCALING_FACTOR_KEY, 1.1);
         }
       else
         ecm_settings_persist (new_state);
 
-      g_settings_set_boolean (new_state_settings, "initialized", TRUE);
+      g_settings_set_boolean (new_state_settings, INITIALIZED_KEY, TRUE);
     }
 
-  g_settings_set_double (interface_settings, "text-scaling-factor", g_settings_get_double (new_state_settings, "text-scaling-factor"));
+  g_settings_set_double (interface_settings,
+                         TEXT_SCALING_FACTOR_KEY,
+                         g_settings_get_double (new_state_settings, TEXT_SCALING_FACTOR_KEY));
 }
 
 static void
 ecm_settings_reset (void)
 {
   {
-    g_autoptr (GSettings) interface_settings = g_settings_new ("org.gnome.desktop.interface");
-    g_settings_reset (interface_settings, "text-scaling-factor");
+    g_autoptr (GSettings) interface_settings = g_settings_new (GNOME_DESKTOP_INTERFACE_SCHEMA);
+    g_settings_reset (interface_settings, TEXT_SCALING_FACTOR_KEY);
   }
 }
